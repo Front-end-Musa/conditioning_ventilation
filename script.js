d79f11658651dd6d7a232eaf6a42156ad3448840
Rufatuly Musa <rufatulymusa567@gmail.com>
2025-05-26 17:56:49 +0100

    changes

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const burgerMenu = document.getElementById('burger-menu');
     const navList = document.querySelector('.nav-list');
+    const burgerBar = document.querySelectorAll('.burger-bar');
 
     // Example: Toggle a class on the body to open/close a menu
     burgerMenu.addEventListener('click', function () {
+        burgerMenu.classList.toggle('menu-open');
         navList.classList.toggle('menu-open');
+        burgerBar.forEach(bar => {
+            bar.classList.toggle('active');
+        });
     });
 });
 
